19144a146a9a0f5fdbf8572e9bd5d25506e994c8
cistT <cist.b220.t.chitose@gmail.com>
2023-05-12 09:24:09 +0900

    UIImageを上下反転もしくは左右反転するメソッドの追加

---
+++ AR-App-practice/utiles/UIImage.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+extension UIImage {
+
+
+    //上下反転
+    func flipVertical() -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        let imageRef = self.cgImage
+        let context = UIGraphicsGetCurrentContext()
+        context?.translateBy(x: 0, y:  0)
+        context?.scaleBy(x: 1.0, y: 1.0)
+        context?.draw(imageRef!, in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        let flipHorizontalImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return flipHorizontalImage!
+    }
+
+    //左右反転
+    func flipHorizontal() -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        let imageRef = self.cgImage
+        let context = UIGraphicsGetCurrentContext()
+        context?.translateBy(x: size.width, y:  size.height)
+        context?.scaleBy(x: -1.0, y: -1.0)
+        context?.draw(imageRef!, in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        let flipHorizontalImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return flipHorizontalImage!
+    }
+
+}
